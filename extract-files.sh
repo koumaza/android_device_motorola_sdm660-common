f0311ad961a584b5f57f09b120e6c079a3adbffe
Hasaber8 <rohanhasabe8@gmail.com>
2019-09-06 22:21:39 +0530

    chef: Add uhid group for fingerprint service--author=Erfan Abdi <erfangplus@gmail.com>
    Change-Id: I28b7b0cf8fec9650555ea094aa22e8ca7b44f903

--- extract-files.sh
+++ extract-files.sh
@@ -82,4 +82,8 @@ sed -i "s|/system/framework/QtiTelephonyServicelibrary.jar|/vendor/framework/Qti
 MOT_GPU_MAPPER="$BLOB_ROOT"/vendor/lib/libmot_gpu_mapper.so
 patchelf --add-needed libui-v28.so "$MOT_GPU_MAPPER"
 
+# Add uhid group for fingerprint service
+FP_SERVICE_RC="$BLOB_ROOT"/vendor/etc/init/android.hardware.biometrics.fingerprint@2.1-service.rc
+sed -i "s/input/uhid input/" "$FP_SERVICE_RC"
+
 "$MY_DIR"/setup-makefiles.sh
