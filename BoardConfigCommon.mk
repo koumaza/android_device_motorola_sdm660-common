54b77316db8f837ef338acc1d425875630131a90
Rohan Hasabe <43720061+Hasaber8@users.noreply.github.com>
2019-09-08 21:38:03 +0530

    Remove Dexpreopt

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -145,17 +145,6 @@ TARGET_USES_COLOR_METADATA := true
 # DRM
 TARGET_ENABLE_MEDIADRM_64 := true
 
-# Enable dexpreopt to speed boot time
-ifeq ($(HOST_OS),linux)
-    ifneq ($(TARGET_BUILD_VARIANT),eng)
-        ifeq ($(WITH_DEXPREOPT),)
-            WITH_DEXPREOPT := true
-            WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
-        endif
-    endif
-endif
-PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
-
 # GPS
 TARGET_NO_RPC := true
 USE_DEVICE_SPECIFIC_GPS := true
